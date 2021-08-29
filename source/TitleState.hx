2ac45ceda69ddc859885b28615a19c658795f155
ManFire100 <87475004+ManFire100@users.noreply.github.com>
2021-08-29 20:04:07 +0200

    More good

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -58,8 +58,6 @@ class TitleState extends MusicBeatState
 
 		super.create();
 
-		NGio.noLogin(APIStuff.API);
-
 		#if ng
 		var ng:NGio = new NGio(APIStuff.API, APIStuff.EncKey);
 		trace('NEWGROUNDS LOL');
@@ -294,7 +292,7 @@ class TitleState extends MusicBeatState
 
 				if (version.trim() != NGio.GAME_VER_NUMS.trim() && !OutdatedSubState.leftState)
 				{
-					FlxG.switchState(new OutdatedSubState());
+					FlxG.switchState(new MainMenuState());
 					trace('OLD VERSION!');
 					trace('old ver');
 					trace(version.trim());
